527954d009c2cc4a015a141f74407841f9ae4a15
Wandonium <hillarywando@gmail.com>
2023-03-13 11:47:10 +0300

    Added a check for correct order of pushing to Stack in the TowerOfHanoi solution.

--- Problems/StackProblems/TowerOfHanoi/Solution.java
+++ Problems/StackProblems/TowerOfHanoi/Solution.java
@@ -1,12 +1,24 @@
 import java.util.Stack;
 public class TowerOfHanoi {
     public static void tower(int numOfDisks, Stack src, Stack aux, Stack dest) {
-        if(numOfDisks == 1) {
+        if(numOfDisks == 1 && check(src, dest)) {
             dest.push(src.pop());
         } else {
             tower(numOfDisks - 1, src, dest, aux);
-            dest.push(src.pop());
+            if(check(src, dest)) dest.push(src.pop());
             tower(numOfDisks - 1, aux, src, dest);
+        }
+    }
+
+    public static boolean check(Stack<Integer> src, Stack<Integer> dest) {
+        if(dest.isEmpty()) {
+            return true;
+        } else {
+            if(src.peek() < dest.peek()) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
@@ -26,6 +38,6 @@ public class TowerOfHanoi {
         System.out.println("After playing game...");
         System.out.println("source stack: " + src);
         System.out.println("auxillary stack: " + aux);
-        System.out.println("destination stack: " + dest);            
+        System.out.println("destination stack: " + dest);
     }
 }
